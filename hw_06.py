12237de9681b069525524c7013502ddb3e93cac8
Majd <majd.tameem.abdallah@gmail.com>
2020-11-01 19:23:53 +0300

    Решение Вопрос 1

--- hw_06.py
+++ hw_06.py
@@ -20,48 +20,17 @@ class Fib():
     8
     """
 
-    def __init__(self, value=0):
+    def __init__(self, value=0,next_value = 1):
         self.value = value
+        self.next_value = next_value
 
     def next(self):
-        "*** ТВОЙ КОД ЗДЕСЬ ***"
+        F = Fib()
+        F.value = self.next_value
+        F.next_value = self.value + self.next_value
+        return F
 
     def __repr__(self):
         return str(self.value)
 
-class VendingMachine:
-    """Торговый автомат, продающий некоторый товар по некоторой цене.
-    
-    >>> v = VendingMachine('яблоко', 10)
-    >>> v.vend()
-    'Товара нет в наличии.'
-    >>> v.restock(2)
-    'Количество товара «яблоко»: 2'
-    >>> v.vend()
-    'Нужно дополнительно внести 10 ₽.'
-    >>> v.deposit(7)
-    'Доступно: 7 ₽'
-    >>> v.vend()
-    'Нужно дополнительно внести 3 ₽.'
-    >>> v.deposit(5)
-    'Доступно: 12 ₽'
-    >>> v.vend()
-    'Получите яблоко и сдачу 2 ₽.'
-    >>> v.deposit(10)
-    'Доступно: 10 ₽'
-    >>> v.vend()
-    'Получите яблоко.'
-    >>> v.deposit(15)
-    'Товара нет в наличии. Вот твои деньги — 15 ₽.'
 
-    >>> w = VendingMachine('лимонад', 2)
-    >>> w.restock(3)
-    'Количество товара «лимонад»: 3'
-    >>> w.restock(3)
-    'Количество товара «лимонад»: 6'
-    >>> w.deposit(2)
-    'Доступно: 2 ₽'
-    >>> w.vend()
-    'Получите лимонад.'
-    """
-    "*** ТВОЙ КОД ЗДЕСЬ ***"
